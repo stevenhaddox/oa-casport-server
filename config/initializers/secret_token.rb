b58f878482ddf45a984072176a75108d818b3ac8
Steven Haddox <steven@haddox.us>
2011-06-29 01:01:41 -0400

    quick rails scaffold app to allow easy mock users from a domain

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cas::Application.config.secret_token = '7dd23638d8346b519b9a6b2cd466682a6fa64a9b345f27156c315e5755f338e4d536186405548a2cf8d6a533aa1906c888cd71a2fc9fcfed179c21d590d95ddd'
---
+++ db/migrate/20110627202346_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :cell
+      t.string :address
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.string :country
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
